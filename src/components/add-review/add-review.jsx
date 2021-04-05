e25217a4b7e3adf8e00f8ff24f636ed23cad7d3d
kuzmich84 <kudim@inbox.ru>
2021-04-05 13:30:24 +0300

    Создает компонент addReview с управляемой формой

--- src/components/add-review/add-review.jsx
+++ src/components/add-review/add-review.jsx
@@ -1,85 +1,110 @@
-import React from 'react';
+import React, {PureComponent} from 'react';
 import PropTypes from "prop-types";
 
-const AddReview = ({film}) => {
-  const {name, posterImage, backgroundImage} = film;
-  return (
-    <section className="movie-card movie-card--full">
-      <div className="movie-card__header">
-        <div className="movie-card__bg">
-          <img src={backgroundImage} alt="The Grand Budapest Hotel"/>
+class AddReview extends PureComponent {
+  constructor(props) {
+    super(props);
+    this.state = {
+      rating: 0,
+      review: ``,
+    };
+    this._handleChangeReview = this._handleChangeReview.bind(this);
+    this._handleChangeRating = this._handleChangeRating.bind(this);
+  }
+
+  _handleChangeReview(evt) {
+    this.setState({review: evt.target.value});
+  }
+
+  _handleChangeRating(evt) {
+    this.setState({rating: +evt.target.value});
+  }
+
+  render() {
+    const {film} = this.props;
+    const {name, posterImage, backgroundImage} = film;
+    return (
+      <section className="movie-card movie-card--full">
+        <div className="movie-card__header">
+          <div className="movie-card__bg">
+            <img src={backgroundImage} alt="The Grand Budapest Hotel"/>
+          </div>
+
+          <h1 className="visually-hidden">WTW</h1>
+
+          <header className="page-header">
+            <div className="logo">
+              <a href="main.html" className="logo__link">
+                <span className="logo__letter logo__letter--1">W</span>
+                <span className="logo__letter logo__letter--2">T</span>
+                <span className="logo__letter logo__letter--3">W</span>
+              </a>
+            </div>
+
+            <nav className="breadcrumbs">
+              <ul className="breadcrumbs__list">
+                <li className="breadcrumbs__item">
+                  <a href="movie-page.html" className="breadcrumbs__link">{name}</a>
+                </li>
+                <li className="breadcrumbs__item">
+                  <a className="breadcrumbs__link">Add review</a>
+                </li>
+              </ul>
+            </nav>
+
+            <div className="user-block">
+              <div className="user-block__avatar">
+                <img src="img/avatar.jpg" alt="User avatar" width="63" height="63"/>
+              </div>
+            </div>
+          </header>
+
+          <div className="movie-card__poster movie-card__poster--small">
+            <img src={posterImage} alt="The Grand Budapest Hotel poster" width="218"
+              height="327"/>
+          </div>
         </div>
 
-        <h1 className="visually-hidden">WTW</h1>
+        <div className="add-review">
+          <form action="#" className="add-review__form">
+            <div className="rating">
+              <div className="rating__stars">
+                <input onChange={this._handleChangeRating} className="rating__input" id="star-1" type="radio" name="rating" value="1"/>
+                <label className="rating__label" htmlFor="star-1">Rating 1</label>
 
-        <header className="page-header">
-          <div className="logo">
-            <a href="main.html" className="logo__link">
-              <span className="logo__letter logo__letter--1">W</span>
-              <span className="logo__letter logo__letter--2">T</span>
-              <span className="logo__letter logo__letter--3">W</span>
-            </a>
-          </div>
+                <input onChange={this._handleChangeRating} className="rating__input" id="star-2" type="radio" name="rating" value="2"/>
+                <label className="rating__label" htmlFor="star-2">Rating 2</label>
 
-          <nav className="breadcrumbs">
-            <ul className="breadcrumbs__list">
-              <li className="breadcrumbs__item">
-                <a href="movie-page.html" className="breadcrumbs__link">{name}</a>
-              </li>
-              <li className="breadcrumbs__item">
-                <a className="breadcrumbs__link">Add review</a>
-              </li>
-            </ul>
-          </nav>
+                <input onChange={this._handleChangeRating} className="rating__input" id="star-3" type="radio" name="rating" value="3"/>
+                <label className="rating__label" htmlFor="star-3">Rating 3</label>
 
-          <div className="user-block">
-            <div className="user-block__avatar">
-              <img src="img/avatar.jpg" alt="User avatar" width="63" height="63"/>
+                <input onChange={this._handleChangeRating} className="rating__input" id="star-4" type="radio" name="rating" value="4"/>
+                <label className="rating__label" htmlFor="star-4">Rating 4</label>
+
+                <input onChange={this._handleChangeRating} className="rating__input" id="star-5" type="radio" name="rating" value="5"/>
+                <label className="rating__label" htmlFor="star-5">Rating 5</label>
+              </div>
             </div>
-          </div>
-        </header>
 
-        <div className="movie-card__poster movie-card__poster--small">
-          <img src={posterImage} alt="The Grand Budapest Hotel poster" width="218"
-            height="327"/>
+            <div className="add-review__text">
+              <textarea
+                onChange={this._handleChangeReview}
+                value={this.state.review}
+                className="add-review__textarea" name="review-text" id="review-text"
+                placeholder="Review text"/>
+              <div className="add-review__submit">
+                <button className="add-review__btn" type="submit">Post</button>
+              </div>
+
+            </div>
+          </form>
         </div>
-      </div>
 
-      <div className="add-review">
-        <form action="#" className="add-review__form">
-          <div className="rating">
-            <div className="rating__stars">
-              <input className="rating__input" id="star-1" type="radio" name="rating" value="1"/>
-              <label className="rating__label" htmlFor="star-1">Rating 1</label>
+      </section>
+    );
+  }
+}
 
-              <input className="rating__input" id="star-2" type="radio" name="rating" value="2"/>
-              <label className="rating__label" htmlFor="star-2">Rating 2</label>
-
-              <input className="rating__input" id="star-3" type="radio" name="rating" value="3"/>
-              <label className="rating__label" htmlFor="star-3">Rating 3</label>
-
-              <input className="rating__input" id="star-4" type="radio" name="rating" value="4"/>
-              <label className="rating__label" htmlFor="star-4">Rating 4</label>
-
-              <input className="rating__input" id="star-5" type="radio" name="rating" value="5"/>
-              <label className="rating__label" htmlFor="star-5">Rating 5</label>
-            </div>
-          </div>
-
-          <div className="add-review__text">
-            <textarea className="add-review__textarea" name="review-text" id="review-text"
-              placeholder="Review text"></textarea>
-            <div className="add-review__submit">
-              <button className="add-review__btn" type="submit">Post</button>
-            </div>
-
-          </div>
-        </form>
-      </div>
-
-    </section>
-  );
-};
 
 export default AddReview;
 
